727962eb6b9d712b93cbe904cd9e49448347e40b
gouthamraj020102 <gouthamraj020102@gmail.com>
2025-11-29 23:12:35 +0530

    Worked with Radio Button

--- user-registration-form-app/src/app/app.component.ts
+++ user-registration-form-app/src/app/app.component.ts
@@ -11,6 +11,14 @@ export class AppComponent {
   defaultCountry = 'india';
   firstname: string | undefined;
   lastname: string | undefined;
+
+  defaultGender = 'Male';
+  gender = [
+    {id: '1', value: 'Male'},
+    {id: '2', value: 'Female'},
+    {id: '3', value: 'Other'}
+  ];
+
   @ViewChild('myForm') form: NgForm | undefined;
 
   onSubmit() {
