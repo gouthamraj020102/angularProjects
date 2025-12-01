1ce41f4c2385d424323ecb3903f9c7ac6dfee704
gouthamraj020102 <gouthamraj020102@gmail.com>
2025-12-01 16:21:04 +0530

    setValue and patchValue methods in Template Driven form

--- user-registration-form-app/src/app/app.component.ts
+++ user-registration-form-app/src/app/app.component.ts
@@ -24,4 +24,29 @@ export class AppComponent {
   onSubmit() {
     console.log(this.form);
   }
+
+  setDefaultValues() {
+    // this.form.value.personDetails.fname = 'Gowtham';
+    // this.form.value.personDetails.lname = 'Raj';
+    // this.form.value.personDetails.email = 'gowtham.raj@gmail.com';
+
+    // this.form?.setValue({
+    //   country: '',
+    //   gender: '',
+    //   hobbies: '',
+    //   personDetails: {
+    //     fname: 'Gowtham',
+    //     lname: 'Raj',
+    //     email: 'gowtham.raj@gmail.com'
+    //   }
+    // });
+
+    this.form?.form.patchValue({
+      personDetails: {
+        fname: 'Gowtham',
+        lname: 'Raj',
+        email: 'gowtham.raj@gmail.com'
+      }
+    });
+  }
 }
